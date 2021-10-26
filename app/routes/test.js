e2d7e2590c0544863f5f66d65b1c02d384957c24
Mn-25b <mn25bp11@gmail.com>
2021-10-26 19:31:49 +0300

    404

--- app/routes/test.js
+++ app/routes/test.js
@@ -10,8 +10,25 @@ router.get("/", (req, res) => {
 });
 // обработка маршрута
 
-router.get("/login", (req, res) => {
-	res.sendFile(path.join(__dirname, "../page/login.html"));
+router.get("/sign_in", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/sign_in.html"));
+});
+router.get("/sign_up", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/sign_up.html"));
+});
+router.get("/user", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/user.html"));
+});
+
+
+router.get("/forget_password_1.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/html password/forget_password_1.html"));
+});
+router.get("/forget_password_2.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/html password/forget_password_2.html"));
+});
+router.get("/forget_password_3.html", (req, res) => {
+	res.sendFile(path.join(__dirname, "../page/html password/forget_password_3.html"));
 });
 
 module.exports = router;
